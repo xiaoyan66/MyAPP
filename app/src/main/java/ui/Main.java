61581a31c4be266195fe231365bdf8631d9c8e37
xiaoyan66 <1587948529@qq.com>
2016-07-05 08:42:54 +0800

    123

--- app/src/main/java/ui/Main.java
+++ app/src/main/java/ui/Main.java
@@ -1,6 +1,6 @@
 package ui;
 
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -13,9 +13,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 
 import fragment.FiveFragment;
 import fragment.FourFragment;
